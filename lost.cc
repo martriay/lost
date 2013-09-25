43a1ffef0cc496352a9f4c96549b786123ec3ec4
martriay <martriay@gmail.com>
2013-09-25 15:58:33 -0300

    Initial commit

---
+++ lost.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+ 
+int main() {
+  using namespace std;
+  int lostNumbers[6] = {4,8,15,16,23,42};
+
+  for (int i = 0; i < 6; i++) {
+    int x;
+    cout << "Enter the required number: ";
+    cin >> x;
+    if ( ! (x == lostNumbers[i]) ) {
+      exit(1);  
+    } else if (i == 5) {
+      main();
+    }
+  }
+}
